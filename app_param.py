49595821b1aafc297a996730ec596034b53ae2d3
RyujiOhgishi1101 <ryuuji.ohgishi@jp.ricoh.com>
2025-03-30 21:24:04 +0900

    モデルをgitignoreに追加。パラメータをapp_paramにまとめ

--- app_param.py
+++ app_param.py
@@ -1,3 +1,10 @@
 WINDYAPI = {
     "APIKEY":"fZEsEmzyEaCXzRKJokgVRoFiXydJRqlp"
 }
+
+REGRESSION_MODEL_PARAM = {
+    "TRAINING_DATA_PATH": "data/training_data.csv",
+    "MODEL_PATH": "models/linear_regression_model.pkl",
+    "SCALER_PATH": "models/scaler.pkl"
+}
+
--- create_model.py
+++ create_model.py
@@ -1,8 +1,9 @@
 from DataManager import RegressionDataManager
 from ModelManager import RegressionModelManager
+import app_param
 
 # データの前処理
-preprocessor = RegressionDataManager('training_data.csv')
+preprocessor = RegressionDataManager(app_param.REGRESSION_MODEL_PARAM["TRAINING_DATA_PATH"])
 preprocessor.plot_data()
 xss_sk, yss_sk = preprocessor.standardize_data()
 
@@ -11,11 +12,10 @@ model_lr = RegressionModelManager(preprocessor.x, preprocessor.y)
 model_lr.train()
 print("Coefficients (no standardization):", model_lr.get_coefficients())
 print("Score (no standardization):", model_lr.get_score())
-model_lr.save_model("model_lr_std_off.pkl")
 
 # 正規化ありのモデル
 model_lr_std = RegressionModelManager(xss_sk, yss_sk)
 model_lr_std.train()
 print("Coefficients (standardized):", model_lr_std.get_coefficients())
 print("Score (standardized):", model_lr_std.get_score())
-model_lr.save_model("model_lr_std_on.pkl")
+model_lr.save_model(app_param.REGRESSION_MODEL_PARAM["MODEL_PATH"])
