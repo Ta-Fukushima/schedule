874985dd6f8ae79799557b5cff25deb4df3d2b22
RyujiOhgishi1101 <ryuuji.ohgishi@jp.ricoh.com>
2025-03-30 21:25:13 +0900

    fix

--- app_param.py
+++ app_param.py
@@ -4,7 +4,6 @@ WINDYAPI = {
 
 REGRESSION_MODEL_PARAM = {
     "TRAINING_DATA_PATH": "data/training_data.csv",
-    "MODEL_PATH": "models/linear_regression_model.pkl",
-    "SCALER_PATH": "models/scaler.pkl"
+    "MODEL_PATH": "models/linear_regression_model.pkl"
 }
 
